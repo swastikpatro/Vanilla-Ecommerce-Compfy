1414e5848fd598e69096db5aa4f167a88b4a9448
swastikpatro <patroswastik.jsdev@gmail.com>
2022-11-04 12:54:24 +0530

    fix: used store to filter featured products

--- index.js
+++ index.js
@@ -9,21 +9,22 @@ import {
   skeletonLoad,
 } from './src/displayProducts.js';
 
+import { setupStore, store } from './src/store.js';
+
 const featuredContainer = getElement('.featured-center');
 
 async function indexInit() {
   featuredContainer.innerHTML = skeletonLoad(3);
   const allProductData = await fetchProducts(allProductsUrl, featuredContainer);
 
+  setupStore(allProductData);
+
   if (allProductData === undefined) {
     return;
   }
 
-  const featuredProducts = allProductData.filter((single) => {
-    const {
-      fields: { featured },
-    } = single;
-
+  const featuredProducts = store.filter((single) => {
+    const { featured } = single;
     return featured;
   });
 
