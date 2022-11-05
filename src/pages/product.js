b14af11e2bfaf344bf97566674e5dba81fbaca25
swastikpatro <patroswastik.jsdev@gmail.com>
2022-11-06 00:58:45 +0530

    fix: fixed the case of non-matching product url id

--- src/pages/product.js
+++ src/pages/product.js
@@ -2,40 +2,42 @@ import '../toggleSidebar.js';
 import '../cart/toggleCart.js';
 import '../cart/setupCart.js';
 
-import { getElement, singleProductUrl } from '../utils.js';
+import { formatPrice, getElement, singleProductUrl } from '../utils.js';
 import { addToCart } from '../cart/setupCart.js';
 const productPage = getElement('.single-product');
-const btn = getElement('.addToCartBtn');
+// const btn = getElement('.addToCartBtn');
 
 function renderSkeletonData() {
   productPage.innerHTML = `
     <div class="section-center single-product-center">
-      <div class="skeleton-single-product-img">
-        <i class="fa-regular fa-image"></i>
-      </div>
+      <div class="section-center single-product-center">
+        <div class="skeleton-single-product-img">
+          <i class="fa-regular fa-image"></i>
+        </div>
 
-      <div class="skeleton-single-product-info">
-        <div>
-          <div class="single-product-title"></div>
-          <div class="single-product-company text-slanted"></div>
-          <div class="skeleton-product-price"></div>
-          <div class="single-product-colors">
-            <div class="skeleton-product-color product-color"></div>
-            <div class="skeleton-product-color product-color"></div>
-            <div class="skeleton-product-color product-color"></div>
+        <div class="skeleton-single-product-info">
+          <div>
+            <div class="skeleton-single-product-title"></div>
+            <div class="skeleton-single-product-company text-slanted"></div>
+            <div class="skeleton-product-price"></div>
+            <div class="skeleton-single-product-colors">
+              <div class="skeleton-product-color product-color"></div>
+              <div class="skeleton-product-color product-color"></div>
+              <div class="skeleton-product-color product-color"></div>
+            </div>
+            <div class="skeleton-single-product-desc">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div class="skeleton-btn">add to cart</div>
           </div>
-          <div class="single-product-desc">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          <div class="addToCartBtn btn" data-id="id">add to cart</div>
         </div>
       </div>
     </div>
@@ -59,7 +61,7 @@ function renderData(data) {
           <div>
             <h2 class="single-product-title">${name}</h2>
             <p class="single-product-company text-slanted">by ${company}</p>
-            <p class="single-product-price">$${price / 100}</p>
+            <p class="single-product-price">${formatPrice(price)}</p>
             <div class="single-product-colors">
               ${colors
                 .map(
@@ -71,7 +73,9 @@ function renderData(data) {
             <p class="single-product-desc">
               ${description}
             </p>
-            <button class="addToCartBtn btn" data-id="${id}">add to cart</button>
+            <button class="addToCartBtn btn" data-id="${id}">
+              add to cart
+            </button>
           </div>
         </article>
       `;
@@ -81,9 +85,11 @@ function renderData(data) {
     ${infoOfProduct}
   </div>
   `;
+
+  return productPage;
 }
 
-async function displaySingleProduct(singleProductUrl) {
+async function fetchSingleProduct(singleProductUrl) {
   const param = new URLSearchParams(window.location.search);
   const productID = param.get('id');
   try {
@@ -103,7 +109,7 @@ async function displaySingleProduct(singleProductUrl) {
     }
 
     const singleData = await response.json();
-    renderData(singleData);
+    return singleData;
   } catch (err) {
     console.log(err);
   }
@@ -111,8 +117,15 @@ async function displaySingleProduct(singleProductUrl) {
 
 renderSkeletonData();
 
-window.addEventListener('DOMContentLoaded', () => {
-  displaySingleProduct(singleProductUrl);
+window.addEventListener('DOMContentLoaded', async () => {
+  const singleProductData = await fetchSingleProduct(singleProductUrl);
+  const productSection = renderData(singleProductData);
+
+  if (!!productSection) {
+    getElement('.addToCartBtn').addEventListener('click', (e) =>
+      addToCart(e.currentTarget.dataset.id)
+    );
+  }
 
   // Following code works
   // window.addEventListener('click', (e) => {
@@ -125,7 +138,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // The below one doesnot work
   // btn = document.querySelector('.btn');
-  btn.addEventListener('click', (e) => {
-    console.log(e.target.dataset.id);
-  });
 });
