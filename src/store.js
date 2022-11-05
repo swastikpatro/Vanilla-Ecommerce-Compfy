6a48543d4b0d9d6b17150bad93f06e5c82527957
swastikpatro <patroswastik.jsdev@gmail.com>
2022-11-05 12:02:29 +0530

    feat: store.js to setStore & store in localstorage

--- src/store.js
+++ src/store.js
@@ -2,15 +2,17 @@ import { getStorageItem, setStorageItem } from './utils.js';
 
 let store = getStorageItem('store');
 const setupStore = (products) => {
-  store = products.map((single) => {
-    const {
-      id,
-      fields: { company, colors, featured, price, name, image },
-    } = single;
+  store = products
+    .map((single) => {
+      const {
+        id,
+        fields: { company, colors, featured, price, name, image },
+      } = single;
 
-    const [{ url: img }] = image;
-    return { id, company, colors, featured, price, name, img };
-  });
+      const [{ url: img }] = image;
+      return { id, company, colors, featured, price, name, img };
+    })
+    .sort(({ price: price1 }, { price: price2 }) => price1 - price2);
 
   setStorageItem('store', store);
 };
