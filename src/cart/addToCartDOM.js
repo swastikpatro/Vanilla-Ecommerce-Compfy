63246b53b5fe19b51108b25d1eb17204856e33ae
swastikpatro <patroswastik.jsdev@gmail.com>
2022-11-06 00:56:46 +0530

    feat: setupcart and added cart functionality

---
+++ src/cart/addToCartDOM.js
@@ -0,0 +1,63 @@
+import { formatPrice, setStorageItem } from '../utils.js';
+
+function getSum(arr) {
+  return arr.reduce((acc, curr) => {
+    acc += curr.count;
+    return acc;
+  }, 0);
+}
+
+function getTotalAmount(arr) {
+  return arr.reduce((acc, curr) => {
+    acc += curr.count * curr.price;
+    return acc;
+  }, 0);
+}
+
+const addToCartDOM = (cartProducts, container, navCart, totalAmountEle) => {
+  container.innerHTML = cartProducts
+    .map((singleProduct) => {
+      const { id, name, price, img, count } = singleProduct;
+      // console.log(amount);
+      return count > 0
+        ? `
+    <article class="cart-item" data-id="${id}" >
+      <img src="${img}" alt="${name}" class="cart-img" />
+  
+      <div class="cart-info">
+        <h4>${name}</h4>
+        <p>${formatPrice(price)}</p>
+        <button class="remove-btn" data-btn>remove</button>
+      </div>
+  
+      <div class="counter">
+        <button class="count-btn increment-count" data-btn>
+          +
+        </button>
+        <span class="cart-count">${count}</span>
+        <button class="count-btn decrement-count" data-btn>
+          -
+        </button>
+      </div>
+    </article>
+    `
+        : '';
+    })
+    .join('');
+
+  const totalCount = getSum(cartProducts);
+  const totalAmount = getTotalAmount(cartProducts);
+
+  // console.log(totalCount);
+  // console.log(totalAmount);
+  navCart.innerText = totalCount;
+  totalAmountEle.innerText = `Total : ${formatPrice(totalAmount)}`;
+  setStorageItem('cartAndOthers', {
+    myCart: cartProducts,
+    totalCountAndAmount: [totalCount, totalAmount],
+  });
+
+  return container;
+};
+
+export default addToCartDOM;
--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -1,5 +1,116 @@
-const addToCart = (id) => {
-  console.log(id);
+import { findProduct, store } from '../store.js';
+import {
+  deepClone,
+  getElement,
+  getStorageItem,
+  setStorageItem,
+} from '../utils.js';
+import addToCartDOM from './addToCartDOM.js';
+import { openCart } from './toggleCart.js';
+
+// const cartItemCountDOM = getElement('.cart-count');
+const cartItemsContainer = getElement('.cart-items');
+const cartItemCountInNav = getElement('.cart-item-count');
+const cartTotalEle = getElement('.cart-total');
+// const cartTotal = getElement('.cart-items');
+
+const cartAndOthers = getStorageItem('cartAndOthers', 'obj');
+let cart = cartAndOthers.myCart ?? [];
+let [totalCount, totalAmount] = cartAndOthers.totalCountAndAmount ?? [];
+
+if (cart.length < 1) {
+  totalCount = 0;
+  cart = [...deepClone(store)];
+  cart.forEach((single) => (single['count'] = 0));
+}
+
+const addToCart = (clickedID) => {
+  openCart();
+  const isInCartAndIndex = cart.findIndex((single) => single.id === clickedID);
+  cart[isInCartAndIndex].count += 1;
+  displayCart(cartItemsContainer, cartItemCountInNav, cartTotalEle);
 };
 
-export { addToCart };
+function getIdFindProductAndHandleDOM({ itemID, toDo, myCart, myContainer }) {
+  const item = findProduct(itemID, myCart);
+  switch (toDo) {
+    case 'remove':
+      item.count = 0;
+      break;
+    case 'increment':
+      item.count++;
+      break;
+    case 'decrement':
+      if (item.count < 0) {
+        item.count = 0;
+      }
+      item.count--;
+      break;
+    default:
+      return;
+  }
+  addToCartDOM(myCart, myContainer, cartItemCountInNav, cartTotalEle);
+}
+
+function handleCartClicks(e) {
+  if (!('btn' in e.target.dataset)) {
+    console.log('clicked outside');
+    return;
+  }
+
+  const itemID = e.target.closest('.cart-item').dataset.id;
+
+  if (e.target.classList.contains('remove-btn')) {
+    getIdFindProductAndHandleDOM({
+      itemID,
+      toDo: 'remove',
+      myCart: cart,
+      myContainer: cartItemsContainer,
+    });
+    return;
+  }
+  if (e.target.classList.contains('increment-count')) {
+    getIdFindProductAndHandleDOM({
+      itemID,
+      toDo: 'increment',
+      myCart: cart,
+      myContainer: cartItemsContainer,
+    });
+    return;
+  }
+  if (e.target.classList.contains('decrement-count')) {
+    getIdFindProductAndHandleDOM({
+      itemID,
+      toDo: 'decrement',
+      myCart: cart,
+      myContainer: cartItemsContainer,
+    });
+    return;
+  }
+}
+
+function displayCart(cartContainerEle, countEle, amountEle) {
+  const cartContainerClickable = addToCartDOM(
+    cart,
+    cartContainerEle,
+    countEle,
+    amountEle
+  );
+
+  if (!!cartContainerClickable) {
+    cartContainerClickable.addEventListener('click', handleCartClicks);
+  }
+}
+
+function displayCartCount() {
+  cartItemCountInNav.innerText = totalCount ?? 0;
+}
+
+function init() {
+  displayCartCount();
+  displayCart(cartItemsContainer, cartItemCountInNav, cartTotalEle);
+}
+
+init();
+
+export { addToCart, cart, displayCart };
