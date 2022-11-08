c83c53cc2ba9ffaf654055de53b60a79a6a815d0
swastikpatro <patroswastik.jsdev@gmail.com>
2022-11-08 09:27:28 +0530

    fix: removed console messages

--- src/sticky-nav.js
+++ src/sticky-nav.js
@@ -5,9 +5,9 @@ const navbarHeight = nav.getBoundingClientRect().height;
 
 window.addEventListener('scroll', (e) => {
   const distanceScrolledFromTop = e.currentTarget.scrollY;
-  console.log(distanceScrolledFromTop, navbarHeight);
+  // console.log(distanceScrolledFromTop, navbarHeight);
   if (distanceScrolledFromTop > navbarHeight) {
-    console.log('entered');
+    // console.log('entered');
     nav.classList.add('nav-sticky');
     return;
   }
